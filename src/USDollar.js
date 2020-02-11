e9ec404cbde48fa523f47cf1a62f664744e6f9c0
raj1020 <raj.1020@hotmail.com>
2020-02-11 22:57:45 +1100

    initial commit for currenct converter React app

---
+++ src/USDollar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class USDollar extends React.Component {
+
+    render () {
+        return (
+            <>
+                <p>Please input the USDollar amount</p>
+                <input value = '' />
+            </>
+        );
+    }
+
+}
+
+export default USDollar;
